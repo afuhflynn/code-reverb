34e1cd28b2446f3186557aa114d7f14f64bb4799
afuhflynn <flyinnsafuh@gmail.com>
2025-12-17 18:27:12 +0100

    feat: add generate review to inngest route

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest";
-import { indexRepo } from "@/inngest/functions";
+import { generateReview, indexRepo } from "@/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [indexRepo],
+  functions: [indexRepo, generateReview],
 });
